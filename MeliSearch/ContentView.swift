0629f9bc600f281ce0ee6dafac3c0d4c59ca6cf7
Sebastian Beltran <beltran.sebastiang@gmail.com>
2025-02-27 11:39:38 -0500

    Initial Commit

---
+++ MeliSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeliSearch
+//
+//  Created by Sebastian Beltran Gonzalez on 27/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeliSearch/MeliSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeliSearchApp.swift
+//  MeliSearch
+//
+//  Created by Sebastian Beltran Gonzalez on 27/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MeliSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
